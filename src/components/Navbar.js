1bb929f505ddfd9d4231efc68440e42138566327
Parikshit Pardeshi <48pardeshi.parikshit2001@gmail.com>
2023-09-09 14:23:16 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,83 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import {
-    Link
-  } from "react-router-dom";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
-  static propTypes = {}
+  static propTypes = {};
 
   render() {
     return (
       <nav className={`navbar navbar-expand-lg navbar-dark bg-dark`}>
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/Navbar">{this.props.title}</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/general">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/business">Business</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/entertainment">Entertainment</Link>
-            </li>
-            <li className="nav-item">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            {this.props.title}
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/general">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  Entertainment
+                </Link>
+              </li>
+              {/* <li className="nav-item">
               <Link className="nav-link" to="/general">General</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/health">Health</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/science">Science</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/sports">Sports</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/technology">Technology</Link>
-            </li>
-          </ul>
-          
+            </li> */}
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  Technology
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-    )
+      </nav>
+    );
   }
 }
 
 Navbar.propTypes = {
-    aboutText: PropTypes.string.isRequired
-}
+  aboutText: PropTypes.string.isRequired,
+};
 
 Navbar.defaultProps = {
-    title: 'Set title here',
-    aboutText: 'About'
-  };
+  title: "Set title here",
+  aboutText: "About",
+};
 
-export default Navbar
+export default Navbar;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
         <div className='my-3'>
-            <div className="card h-100">
-
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+            <div className="card">
+                <img src={imageUrl} className="card-img-top" alt="..." style={{height: '300px'}}/>
                 <div className="card-body">
-                    <h5 className="card-title">{title}....</h5>
+                    <h5 className="card-title"><span className="badge text-bg-danger mb-2 my-1">{source}</span><br/>{title}....</h5>
                     <p className="card-text">{description}....</p>
+                    <p className="card-text"><small className="text-muted"><b>By {!author?"Unknown":author}</b><br/>{new Date(date).toGMTString()}</small></p>
                     <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark d-flex justify-content-center">Read More</a>
                 </div>
             </div>
